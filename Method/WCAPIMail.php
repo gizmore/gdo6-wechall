048e77984f1627d6f1ab42568eb41d6d6fb872eb
gizmore <gizmore@wechall.net>
2017-10-05 20:12:46 +0200

    6.0.4_fixpack

--- Method/WCAPIMail.php
+++ Method/WCAPIMail.php
@@ -1,19 +1,36 @@
 <?php
 namespace GDO\WeChall\Method;
-
 use GDO\GWF\MethodAjax;
-use GDO\User\GDO_User;
-use GDO\Util\Common;
 use GDO\WeChall\WC_Site;
-
+use GDO\User\GDO_User;
+use GDO\DB\GDO;
+use GDO\Util\Common;
+/**
+ * Own implementation of the WeChall API.
+ * The mail part checks if an account exists.
+ * We also use %AUTHKEY% to prevent enumeration. 
+ * @author gizmore
+ * @version 7.00
+ * @since 1.00
+ */
 final class WCAPIMail extends MethodAjax
 {
     public function execute()
     {
-        $wechall = WC_Site::getWeChall();
-        
-        
-        $result = GDO_User::getByName(Common::getGetString('user'))
-        die($result);
+        if ($this->checkAuthKey(Common::getGetString('authkey')))
+        {
+            $onsitename = GDO::quoteS(Common::getGetString('user'));
+            $onsitemail = GDO::quoteS(Common::getGetString('email'));
+            if (GDO_User::table()->select('1')->where("user_name=$onsitename AND user_email=$onsitemail AND user_deleted_at IS NULL")->exec()->fetchValue())
+            {
+                die('1');
+            }
+        }
+        die('0');
+    }
+    
+    public function checkAuthKey(string $authkey)
+    {
+        return WC_Site::getWeChall()->getVar('site_authkey') === $authkey;
     }
 }
