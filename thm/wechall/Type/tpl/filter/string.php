d36e145494a2f758ceed61fd359be83f9626b404
gizmore <gizmore@wechall.net>
2020-10-11 13:19:35 +0200

    6.10-r9100

--- thm/wechall/Type/tpl/filter/string.php
+++
@@ -1,9 +0,0 @@
-<?php 
-use GDO\DB\GDT_String;
-$field instanceof GDT_String;
-?>
-<input
- name="f[<?= $field->name?>]"
- type="text"
- size="<?= min($field->max, 20); ?>"
- value="<?= $field->displayFilterValue(); ?>" />
