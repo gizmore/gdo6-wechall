0b6a52ccfccfc03271de187c18a7f1bbbd2befd9
gizmore <gizmore@wechall.net>
2020-10-25 09:41:12 +0100

    6.10-r9309_guestbook

--- GDT/WC_SiteName.php
+++ GDT/WC_SiteName.php
@@ -2,6 +2,10 @@
 namespace GDO\WeChall\GDT;
 use GDO\DB\GDT_String;
 
+/**
+ * A sitename identifier.
+ * @author gizmore
+ */
 final class WC_SiteName extends GDT_String
 {
     public function __construct()
@@ -9,4 +13,5 @@ final class WC_SiteName extends GDT_String
         $this->min(2)->max(32);
         $this->caseS()->utf8();
     }
+    
 }
