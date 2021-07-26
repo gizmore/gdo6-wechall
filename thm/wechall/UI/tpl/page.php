73007b70c0326e924b609fbc37e060e52411db90
gizmore <gizmore@wechall.net>
2021-07-26 09:16:57 +0200

    6.10.4-r6040_meta_robots

--- thm/wechall/UI/tpl/page.php
+++ thm/wechall/UI/tpl/page.php
@@ -8,11 +8,10 @@ use GDO\Javascript\Module_Javascript;
 <!DOCTYPE html>
 <html>
   <head>
-    <? # Website::displayMeta(); ?>
+    <?= Website::displayMeta(); ?>
     <?= Website::displayLink(); ?>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="robots" content="index, follow" />
   </head>
   <body>
     <?= GDT_Template::php('WeChall', 'layout/top_bar.php'); ?>
